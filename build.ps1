1bda21338dfcc25ed4fda5f762db08131e902604
kapryeong <kap@tecalog.com>
2020-10-19 15:53:39 +0900

    빌드스크립트 조정

--- build.ps1
+++ build.ps1
@@ -2,7 +2,5 @@ $env:BASYX_REPO = "$(Get-Location)\basyx-packages"
 mkdir basyx-packages -erroraction 'silentlycontinue'
 Get-ChildItem $env:USERPROFILE\.nuget -Directory -Filter basyx* -Recurse | Remove-Item -force -Recurse
 dotnet build .\src\submodules\basyx\sdks\dotnet\basyx-core\BaSyx.Core.sln
-dotnet build .\src\submodules\basyx\sdks\dotnet\basyx-components\BaSyx.AAS.Client.Http\BaSyx.AAS.Client.Http.csproj
-dotnet build .\src\submodules\basyx\sdks\dotnet\basyx-components\BaSyx.Components.sln
 
 dotnet build Mos.sln
